5084f43c0375dda889d352e8ef13f13e5eaaceb3
Yunus Emre Duran <107029284+yunuseduran@users.noreply.github.com>
2024-09-05 22:22:57 +0300

    Add files via upload

---
+++ parola.py
@@ -0,0 +1,8 @@
+import random
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+sifre_uzunlugu = int(input("Sifre uzunlugu: "))
+sifre =""
+for i in range(sifre_uzunlugu):
+    sifre += random.choice(karakterler)
+
+print(sifre)
